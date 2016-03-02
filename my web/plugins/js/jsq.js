7ee1743f463a59f307801add4d0ae59d38352461
dhwanilvyas <dhwanilvyas@outlook.com>
2016-03-02 21:02:28 +0530

    Got the preloader working

--- my web/plugins/js/jsq.js
+++ my web/plugins/js/jsq.js
@@ -1,10 +1,6 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
-
-
 $(document).ready(function() {
     setTimeout(function() {
 
-            $('#preloader').css({display-none});
+            $('#preloader').fadeOut('slow');
         });
     }, 1000);
