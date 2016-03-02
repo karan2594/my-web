658467a267b53df27653c51289c71b68fa8c6829
Karan Mehta <ktmehta1994@outlook.com>
2016-03-02 20:18:03 +0530

    Added files via upload

---
+++ my web/plugins/js/jsq.js
@@ -0,0 +1,10 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+
+            $('#preloader').css({display-none});
+        });
+    }, 1000);
